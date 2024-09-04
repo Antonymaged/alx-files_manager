735165d092f92f4c49244c2731f01838e21c1fac
Antonymaged <magedantony1@gmail.com>
2024-09-03 19:15:58 -0700

    utils/

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,14 @@
-import { mongodb } from 'mongodb';
-import envLoader from './env_loader';
+import mongodb from 'mongodb';
+
+const { MongoClient } = mongodb;
 
 class DBClient {
 	constructor() {
 		this.host = process.env.DB_HOST || 'localhost';
 		this.port = process.env.DB_PORT || 27017;
 		this.database = process.env.DB_DATABASE || 'files_manager';
-		this.uri = `mongodb://${host}:${port}/${database}`;
-		this.client = new mongodb.MongoClient(uri, { useUnifiedTopology: true });
+		this.uri = `mongodb://${this.host}:${this.port}/${this.database}`;
+		this.client = new MongoClient(this.uri, { useUnifiedTopology: true });
 		this.client.connect
 	}
 
