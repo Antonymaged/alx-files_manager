cc6a76ae718d57476cdd31249fde9aacb15225aa
Antonymaged <magedantony1@gmail.com>
2024-09-03 22:34:02 -0700

    fu bit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth.js';
 
 const router = express.Router();
@@ -9,5 +10,7 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+router.get('/connect', basicAuthenticate, AuthController.getConnect);
+router.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
 
 export default router;
