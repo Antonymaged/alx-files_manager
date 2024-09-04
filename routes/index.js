f2ec1a2cf22b23b1b79b26f2c57aa94db364d6a2
Antonymaged <magedantony1@gmail.com>
2024-09-03 20:40:36 -0700

    i can do it

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
-import UsersController from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController.js';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
 export default router;
