c6ceedda7f17d7ece2209d01c8ad1ec4837d3029
Antonymaged <magedantony1@gmail.com>
2024-09-03 20:14:04 -0700

    ez

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import router from './routes/index.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', router)
+app.listen(port, () => {console.log('ez')});
