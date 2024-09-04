9398ed0bf4c48be0ddd027431501c041b459c528
Antonymaged <magedantony1@gmail.com>
2024-09-03 21:08:50 -0700

    f u

---
+++ utils/user.js
@@ -0,0 +1,25 @@
+import redisClient from './redis.js';
+import dbClient from './db.js';
+
+const userUtils = {
+  async getUserIdAndKey(request) {
+    const obj = { userId: null, key: null };
+
+    const xToken = request.header('X-Token');
+
+    if (!xToken) return obj;
+
+    obj.key = `auth_${xToken}`;
+
+    obj.userId = await redisClient.get(obj.key);
+
+    return obj;
+  },
+
+  async getUser(query) {
+    const user = await dbClient.usersCollection.findOne(query);
+    return user;
+  },
+};
+
+export default userUtils;
