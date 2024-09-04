7d1b85e5a4bd02b476a19fa5a931f39473390e4f
Antonymaged <magedantony1@gmail.com>
2024-09-03 19:34:25 -0700

    i haaaaaate alx

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,15 @@
 import mongodb from 'mongodb';
 // eslint-disable-next-line no-unused-vars
-import Collection from 'mongodb/lib/collection';
-import envLoader from './env_loader';
+
+const { MongoClient } = mongodb;
 
 class DBClient {
 	constructor() {
-		envLoader();
 		const host = process.env.DB_HOST || 'localhost';
 		const port = process.env.DB_PORT || 27017;
 		const database = process.env.DB_DATABASE || 'files_manager';
 		const url = `mongodb://${host}:${port}/${database}`;
-		this.client = new mongodb.MongoClient(url, { useUnifiedTopology: true });
+		this.client = new MongoClient(url, { useUnifiedTopology: true });
 		this.client.connect();
 	}
 
