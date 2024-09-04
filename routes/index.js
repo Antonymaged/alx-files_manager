f65abc53fd054990a09bdc6978b87c1e0cb8d30c
Antonymaged <magedantony1@gmail.com>
2024-09-03 21:58:00 -0700

    fu albitc

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import { getUserFromXToken, getUserFromAuthorization } from '../utils/auth.js';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
+router.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
 export default router;
