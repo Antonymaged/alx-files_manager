7bb217ae562ba949886880daa48995299a6686b7
Antonymaged <magedantony1@gmail.com>
2024-09-03 21:49:04 -0700

    fu falx

--- utils/redis.js
+++ utils/redis.js
@@ -1,27 +1,32 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
+
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.connected = true;
+    this.isClientConnected = true;
     this.client.on('error', (err) => {
       console.error('Redis client failed to connect:', err.message || err.toString());
-      this.connected = false;
+      this.isClientConnected = false;
     });
     this.client.on('connect', () => {
-      this.connected = true;
+      this.isClientConnected = true;
     });
   }
+
   isAlive() {
-    return this.connected;
+    return this.isClientConnected;
   }
+
   async get(key) {
     return promisify(this.client.GET).bind(this.client)(key);
   }
+
   async set(key, value, duration) {
     await promisify(this.client.SETEX)
       .bind(this.client)(key, duration, value);
   }
+
   async del(key) {
     await promisify(this.client.DEL).bind(this.client)(key);
   }
