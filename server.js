ef7f06dcfc19412c525c3e7b3ca6bf719a8ef1ac
Antonymaged <magedantony1@gmail.com>
2024-09-03 20:29:51 -0700

    server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/', router)
-app.listen(port, () => {console.log('ez')});
+app.listen(port, () => {console.log('Server running on port 5000')});
