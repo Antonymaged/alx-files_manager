3292d65ab78bc1ed6edda48bbc9d3cbfda2b8b6f
Antonymaged <magedantony1@gmail.com>
2024-09-03 23:34:55 -0700

    why

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import startServer from './libs/boot';
 import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
 
 const server = express();
+const port = process.env.PORT || 5000;
 
 injectMiddlewares(server);
 injectRoutes(server);
-startServer(server);
+
+server.listen(port, () => console.log('Server running on port 5000'))
 
 export default server;
